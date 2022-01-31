2f5fe9c1296775b2dd09fc720383b230aac9465b
Dedi Arief Wibisono <dedi.wibisono17@gmail.com>
2022-01-31 14:40:08 +0700

    Add files via upload

--- js/project.js
+++ js/project.js
@@ -4,6 +4,8 @@ $.ajax({
     success: function(response) {
         console.log(response);
         var a;
+        var location = '/kertakayu/gallery.html';
+
         for (a = 0; a < response.length; a++) {
             $(".project").append(`
             <div class="col-md-3 col-6">
@@ -11,6 +13,7 @@ $.ajax({
                     <div class="card__circle">
                         <div class="image image-data" data-toggle="modal" data-target="#logo" data-help="${response[a].id - 1}">
                             <img src="${response[a].img}" alt="${response[a].title}" title="${response[a].title}" loading="lazy">
+                            <div class="font-std">${response[a].title}</div>
                         </div>
                     </div>
                     
@@ -26,6 +29,7 @@ $.ajax({
                     <div class="modal-body">
                         <h3 class="title-purple font-medium text-center">${response[data].title}</h3>
                         <div class="image"><img src="${response[data].img}" alt="${response[data].title}" title="${response[data].title}" loading="lazy"></div>
+                        <p class="mt-2 font-std">${response[data].desc}</p>
                         <button type="button" class="btn btn-light float-right" data-dismiss="modal">Close</button>
                     </div>
                 `);
