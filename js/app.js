b072c4434cf2a3a5c3e462fcf48fc5a8f475b8cf
Dedi Arief Wibisono <dedi.wibisono17@gmail.com>
2022-02-21 17:22:56 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -143,13 +143,13 @@ $.ajax({
             dots: false,
             infinite: false,
             speed: 300,
-            slidesToShow: 4,
+            slidesToShow: 3,
             slidesToScroll: 1,
             adaptiveHeight: true,
             responsive: [{
                     breakpoint: 1024,
                     settings: {
-                        slidesToShow: 4,
+                        slidesToShow: 3,
                         slidesToScroll: 1,
                         infinite: true,
                         dots: true
